07d57f3c7edb057d7dfdc1fabeaaa6b1beafcc77
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-12-01 18:35:47 -0700

    bootstrap, form setup

---
+++ src/app/components/reactive-form/reactive-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: false,
+  templateUrl: './reactive-form.html',
+  styleUrl: './reactive-form.css',
+})
+export class ReactiveForm {
+
+}
